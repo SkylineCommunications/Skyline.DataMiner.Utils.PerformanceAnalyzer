cef903233d01c4303cc7a813e76d7958cea07166
Edib Šupić <edib.supic@skyline.be>
2025-05-06 12:21:37 +0200

    Unit tests fix.

--- PerformanceAnalyzerTests/PerformanceTrackerTests.cs
+++ PerformanceAnalyzerTests/PerformanceTrackerTests.cs
@@ -39,7 +39,7 @@
 			var trackedMethod = tracker.TrackedMethod;
 
 			// Assert
-			Assert.IsNull(trackedMethod);
+			Assert.IsNotNull(trackedMethod);
 			Assert.AreEqual("PerformanceTrackerTests", trackedMethod.ClassName);
 			Assert.AreEqual("PerformanceTracker_Initialize_ShouldTrackMethod", trackedMethod.MethodName);
 		}
